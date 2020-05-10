161c6137f9dc055918425b07a9f49df74cc21eb1
霍荣慧 <huorh@missfresh.cn>
2020-05-10 12:17:05 +0800

    优化客户端

--- src/main/cli-go-to-net.go
+++ src/main/cli-go-to-net.go
@@ -9,7 +9,7 @@ import (
 
 // serverCommandUsage 打印控制台Usage信息
 func clientCommandUsage() {
-    _, _ = fmt.Fprintln(flag.CommandLine.Output(), "cli-go-to-net -H Hostname -u USERNAME -P PASSWORD [options]")
+    _, _ = fmt.Fprintln(flag.CommandLine.Output(), "cli-go-to-net -sh Hostname -u USERNAME -p PASSWORD [options]")
 
     flag.PrintDefaults()
 }
@@ -17,13 +17,13 @@ func clientCommandUsage() {
 func main() {
     server := socket.Socket{}
 
-    flag.StringVar(&server.Hostname, "H", "", "服务器域名")
-    flag.IntVar(&server.Port, "p", 443, "服务器端口")
-    flag.StringVar(&server.LocalAddr, "l", "127.0.0.1", "本地监听地址")
+    flag.StringVar(&server.Hostname, "sh", "", "服务器域名")
+    flag.IntVar(&server.Port, "sp", 443, "服务器端口")
+    flag.StringVar(&server.LocalAddr, "la", "127.0.0.1", "本地监听地址")
     flag.IntVar(&server.LocalPort, "lp", 1280, "本地监听端口")
 
     flag.StringVar(&server.Username, "u", "", "服务端身份认证用户名")
-    flag.StringVar(&server.Password, "P", "", "服务端身份认证密码")
+    flag.StringVar(&server.Password, "p", "", "服务端身份认证密码")
 
     flag.BoolVar(&server.Verbose, "v", false, "打印详细日志")
 
