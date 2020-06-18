3b9c2f1ffa01ed926059d19f8e73556446d2df16
Luna <luna@cyl-mail.com>
2020-06-18 19:01:27 +0800

    优化代码

--- src/utils/os.go
+++ src/utils/os.go
@@ -6,6 +6,7 @@ import (
     "io"
     "os"
     "os/exec"
+    "runtime"
     "strings"
 )
 
@@ -125,4 +126,36 @@ func ExecCommandOutputToLog(name string, args []string, env *[]string) error {
     }
 
     return nil
+}
+
+// GetOsType 获取文件系统类型：debian/redhat/windows/darwin/unknown
+func GetOsType() (string, error) {
+    switch runtime.GOOS {
+    case "darwin":
+        return runtime.GOOS, nil
+    case "linux":
+        // ubuntu视为debian
+        if debian, err := FileExists("/etc/debian_version"); nil != err || debian {
+            if debian {
+                return "debian", nil
+            }
+
+            return "", err
+        }
+
+        // centos/fedora视为redhat
+        if redhat, err := FileExists("/etc/redhat-version"); nil != err || redhat {
+            if redhat {
+                return "redhat", nil
+            }
+
+            return "", err
+        }
+
+        return "unknown", nil
+    case "windows":
+        return runtime.GOOS, nil
+    default:
+        return "unknown", nil
+    }
 }
